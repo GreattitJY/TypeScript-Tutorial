ef1494b93a75c522794e28a22f86574cf422f3a2
GreattitJY <great.tit.jaejae@gmail.com>
2023-02-07 16:44:58 +0900

    docs: Intersection-Types

---
+++ Section-07-Advanced-Types/Intersection-Types.js
@@ -0,0 +1,12 @@
+var e = {
+    id: 100,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: "(408)-897-5684"
+};
+var c = {
+    name: "ABC Inc.",
+    credit: 100000,
+    email: "sales@abcinc.com",
+    phone: "(408)-897-5735"
+};
---
+++ Section-07-Advanced-Types/Intersection-Types.ts
@@ -0,0 +1,31 @@
+interface BusinessPartner {
+  name: string;
+  credit: number;
+}
+
+interface Identity {
+  id: number;
+  name: string;
+}
+
+interface Contact {
+  email: string;
+  phone: string;
+}
+
+type Employee = Identity & Contact;
+type Customer = BusinessPartner & Contact;
+
+let e: Employee = {
+  id: 100,
+  name: "John Doe",
+  email: "john.doe@example.com",
+  phone: "(408)-897-5684",
+};
+
+let c: Customer = {
+  name: "ABC Inc.",
+  credit: 100000,
+  email: "sales@abcinc.com",
+  phone: "(408)-897-5735",
+};
